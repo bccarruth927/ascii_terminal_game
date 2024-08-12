500bb1261ff98dacdf7a0dc5a1bdc37faa6f562b
bccarruth927 <brecarr1997@aol.com>
2024-08-12 08:38:34 -0400

    Add methods to Player class

--- ascii_game.py
+++ ascii_game.py
@@ -56,6 +56,96 @@ class Player:
             description += ' Fortunately you have not yet died! You are {age} years of age.'.format(age=self.age)
         
         return description
+    
+    def player_dies(self, monster):
+        # If the player loses all of their health then the is_dead attribute is switched to false
+        self.is_dead = True
+
+        # A negative value could potentially result in a bug where the player continues to live despite having 0 health
+        # Set health to 0 to prevent a bug possibility
+        if self.health != 0:
+            self.health = 0
+        
+        print('Oh no! {name} died fighting {monster}!'.format(name=self.name, monster=monster.name))
+
+    def gain_health(self):
+        # If the player successfully defeats a monster, increase their health by 2
+        self.health += 2
+
+    def lose_health(self, monster):
+        # Health lost depends on the combat class of the Player and the type of monster being fought
+        if self.combat_class == 'warrior' and monster.name == 'ogre':
+            self.health -= monster.strength / 5
+            print('Your training as a {combat_class} pays off! The {monster}\'s attack is reduced significantly'.format(combat_class=self.combat_class.title(), monster=monster.name.title()))
+        elif self.combat_class == 'warrior' and monster.name == 'goblin':
+            self.health -= monster.strength
+            print('The {monster} proves to be more nimble than you. You take full damage.'.format(monster=monster.name.title()))
+        elif self.combat_class == 'warrior' and monster.name == 'spectre':
+            self.health -= monster.strength / 2
+            print('Your training as a {combat_class} has provided you with mental fortitude. The {monster}\'s attack is reduced by half'.format(combat_class=self.combat_class.title(), monster=monster.name.title()))
+        elif self.combat_class == 'archer' and monster.name == 'ogre':
+            self.health -= monster.strength / 2
+            print('You\'re long range attack style proves troublesome for the {monster}. {monster}\'s attack is reduced by half'.format(monster=monster.name.title()))
+        elif self.combat_class == 'archer' and monster.name == 'goblin':
+            self.health -= monster.strength / 5
+            print('You\'re naturally dexterous making it easy to dodge the {monster}\'s attacks. {monster}\'s attack is significantly reduced'.format(monster=monster.name.title()))
+        elif self.combat_class == 'archer' and monster.name == 'spectre':
+            self.health -= monster.strength
+            print('You have little to no defense against the {monster}. You take full damage.'.format(monster=monster.name.title()))
+        elif self.combat_class == 'mage' and monster.name == 'ogre':
+            self.health -= monster.strength
+            print('You\'re magical training has left you ill prepared for the brute force of the {monster}. You take full damage.'.format(monster=monster.name.title()))
+        elif self.combat_class == 'mage' and monster.name == 'goblin':
+            self.health -= monster.strength / 2
+            print('You\'re lack of physical armor leaves you light on your feet. {monster}\'s attack is reduced by half.'.format(monster=monster.name.title()))
+        elif self.combat_class == 'mage' and monster.name == 'spectre':
+            self.health -= monster.strength / 5
+            print('Years of magical training as {combat_class} prepares you for otherwordly attacks! {monster}\'s attack is significantly reduced.'.format(combat_class=self.combat_class.title(), monster=monster.name.title()))
+        else:
+            # If on the chance of a bug and neither of the above condition are met. Bless the Player by divine intervention
+            print('By a divine miracle no damage befalls {name}.'.format(self.name))
+    
+    def attack_monster(self, monster):
+        # Attack strength depends on the combat class of the Player and the monster type
+        if self.combat_class == 'warrior' and monster.name == 'ogre':
+            monster.health -= self.strength
+            print('You attack the {monster} for {damage} points of damage.'.format(monster=monster.name.title(), damage=self.strength))
+        elif self.combat_class == 'warrior' and monster.name == 'goblin':
+            monster.health -= self.dexterity
+            print('You attack the {monster} for {damage} points of damage.'.format(monster=monster.name.title(), damage=self.dexterity))
+        elif self.combat_class == 'warrior' and monster.name == 'spectre':
+            monster.health -= self.wisdom
+            print('You attack the {monster} for {damage} points of damage.'.format(monster=monster.name.title(), damage=self.wisdom))
+        elif self.combat_class == 'archer' and monster.name == 'ogre':
+            monster.health -= self.dexterity
+            print('You attack the {monster} for {damage} points of damage.'.format(monster=monster.name.title(), damage=self.dexterity))
+        elif self.combat_class == 'archer' and monster.name == 'goblin':
+            monster.health -= self.strength
+            print('You attack the {monster} for {damage} points of damage.'.format(monster=monster.name.title(), damage=self.strength))
+        elif self.combat_class == 'archer' and monster.name == 'spectre':
+            monster.health -= self.wisdom
+            print('You attack the {monster} for {damage} points of damage.'.format(monster=monster.name.title(), damage=self.wisdom))
+        elif self.combat_class == 'mage' and monster.name == 'ogre':
+            monster.health -= self.strength
+            print('You attack the {monster} for {damage} points of damage.'.format(monster=monster.name.title(), damage=self.strength))
+        elif self.combat_class == 'mage' and monster.name == 'goblin':
+            monster.health -= self.dexterity
+            print('You attack the {monster} for {damage} points of damage.'.format(monster=monster.name.title(), damage=self.dexterity))
+        elif self.combat_class == 'mage' and monster.name == 'spectre':
+            monster.health -= self.wisdom
+            print('You attack the {monster} for {damage} points of damage.'.format(monster=monster.name.title(), damage=self.wisdom))
+        else:
+            # If on the chance of a bug and neither of the conditions above are met. Blame divine punishment
+            print('Is is divine punishment? No damage befalls the {monster}'.format(monster=monster.name))
+
+    def boost_stats(self, monster):
+        # Boosted stat depends on the monster fought
+        if monster.name == 'ogre':
+            self.strength += 2
+        elif monster.name == 'goblin':
+            self.dexterity += 2
+        else:
+            self.wisdom += 2
 
 # Test whether Player class initializes correctly
 player_character = Player('The Mighty Brenden', 26, 'human', 'warrior')
